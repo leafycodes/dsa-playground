eb06e9adc69ac181f483cff216b4c18611476ef3
Anish Kumar Barik <barikanish0@gmail.com>
2025-05-26 22:20:43 +0530

    best time to buy and sell stocks

---
+++ dsa/01-Arrays/LeetCode/maxProfit.java
@@ -0,0 +1,12 @@
+// best time to buy and sell stocks
+// Time Comp: O(n)
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buy = 0, profit = 0;
+
+        // profit = sell - buy
+
+        return profit;
+    }
+}
