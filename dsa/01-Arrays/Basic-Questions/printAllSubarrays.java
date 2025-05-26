f08bf266b4a46aceefd5fdf35e7c62f0f4bc0610
Anish Kumar Barik <barikanish0@gmail.com>
2025-05-26 15:05:18 +0530

    geenrate all subarrays

---
+++ dsa/01-Arrays/Basic-Questions/printAllSubarrays.java
@@ -0,0 +1,21 @@
+public class printAllSubarrays {
+    public static void main(String[] args) {
+        int[] arr = { 2, 56, 7, 43, 42 };
+        int subarrays = 0;
+
+        // Time Complexity: O(n^3)
+        for (int i = 0; i < arr.length; i++) {
+            // j starts from i to include the same element as i, as a single elemetn is also
+            // a subarray
+            for (int j = i; j < arr.length; j++) {
+                for (int k = i; k <= j; k++) {
+                    System.out.print(arr[k] + " ");
+                }
+                System.out.println();
+                subarrays++;
+            }
+            System.out.println();
+        }
+        System.out.println("total subarrays: " + subarrays);
+    }
+}
