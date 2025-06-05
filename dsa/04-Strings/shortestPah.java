c8d4ab1463f59e89bed06b708f3df33e01ec1c37
Anish Kumar Barik <barikanish0@gmail.com>
2025-06-05 13:58:01 +0530

    displacement after travelling

---
+++ dsa/04-Strings/shortestPah.java
@@ -0,0 +1,25 @@
+public class shortestPah {
+    public static void main(String[] args) {
+        String path = "WNEENESENNN";
+        System.out.println(getDisplacement(path));
+    }
+
+    private static double getDisplacement(String path) {
+        int x = 0, y = 0;
+
+        for (int i = 0; i < path.length(); i++) {
+            if (path.charAt(i) == 'N') {
+                y++;
+            } else if (path.charAt(i) == 'S') {
+                y--;
+            } else if (path.charAt(i) == 'W') {
+                x--;
+            } else {
+                x++;
+            }
+        }
+        int x2 = x * x;
+        int y2 = y * y;
+        return Math.sqrt(x2 + y2);
+    }
+}
