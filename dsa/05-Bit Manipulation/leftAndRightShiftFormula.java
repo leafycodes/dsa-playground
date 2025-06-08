19fced71b104507a4ce95f83ea5113c278679e97
Anish Kumar Barik <barikanish0@gmail.com>
2025-06-09 00:14:53 +0530

    good to know formula for binary shift

---
+++ dsa/05-Bit Manipulation/leftAndRightShiftFormula.java
@@ -0,0 +1,9 @@
+public class leftAndRightShiftFormula {
+    public static void main(String[] args) {
+        // a >> b = a / 2^b
+        System.out.println(6 >> 1);
+
+        // a << b = a * 2^b
+        System.out.println(6 << 1);
+    }
+}
