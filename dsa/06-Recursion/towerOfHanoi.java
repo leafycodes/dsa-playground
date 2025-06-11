858b515c5117c1fa0457805730c3f43290b9b0ee
Anish Kumar Barik <barikanish0@gmail.com>
2025-06-11 13:41:59 +0530

    tower of hanoi

---
+++ dsa/06-Recursion/towerOfHanoi.java
@@ -0,0 +1,17 @@
+public class towerOfHanoi {
+    public static void main(String[] args) {
+        int n = 3;
+        towerOfHanoiSteps(n, "A", "B", "C");
+    }
+
+    private static void towerOfHanoiSteps(int n, String src, String helper, String dest) {
+        if (n == 0) {
+
+            return;
+        }
+
+        towerOfHanoiSteps(n - 1, src, dest, helper);
+        System.out.println("move disk " + n + " from " + src + " to " + dest);
+        towerOfHanoiSteps(n - 1, helper, src, dest);
+    }
+}
