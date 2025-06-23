a9cc3e138798a96689d446b119274dfb11cba7c9
Anish Kumar Barik <barikanish0@gmail.com>
2025-06-23 23:14:08 +0530

    queue using linkedList

--- dsa/12-Queues/Implementation/linkedListImplementation.java
+++ dsa/12-Queues/Implementation/linkedListImplementation.java
@@ -60,8 +60,6 @@ public class linkedListImplementation {
         q.add(2);
         q.add(3);
         q.add(4);
-        q.add(5);
-        q.add(6);
 
         while (!q.isEmpty()) {
             System.out.print(q.peek() + " ");
