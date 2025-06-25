dccfb5c9bb72ccf1b992e05d67b4357019dca5d7
Anish Kumar Barik <barikanish0@gmail.com>
2025-06-25 14:15:50 +0530

    sliding window maximum

--- dsa/12-Queues/LeetCode/maxOfSubarrays.java
+++ dsa/12-Queues/LeetCode/maxOfSubarrays.java
@@ -33,3 +33,46 @@ class Solution {
         return result;
     }
 }
+
+// sliding window - leetcode
+
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        ArrayList<Integer> list = maxOfSubarrays(nums, k);
+
+        int[] res = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            res[i] = list.get(i);
+        }
+
+        return res;
+    }
+
+    private ArrayList<Integer> maxOfSubarrays(int[] arr, int k) {
+        ArrayList<Integer> res = new ArrayList<>();
+        Deque<Integer> dq = new ArrayDeque<>();
+
+        for (int i = 0; i < k; i++) {
+            while (!dq.isEmpty() && arr[dq.peekLast()] <= arr[i]) {
+                dq.removeLast();
+            }
+            dq.addLast(i);
+        }
+
+        res.add(arr[dq.peekFirst()]);
+
+        for (int i = k; i < arr.length; i++) {
+            while (!dq.isEmpty() && dq.peekFirst() <= i - k) {
+                dq.removeFirst();
+            }
+
+            while (!dq.isEmpty() && arr[dq.peekLast()] <= arr[i]) {
+                dq.removeLast();
+            }
+            dq.addLast(i);
+            res.add(arr[dq.peekFirst()]);
+        }
+
+        return res;
+    }
+}
