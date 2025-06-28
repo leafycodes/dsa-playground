98fd4c55c308fa70aac8453771d72c46fd725b9b
Anish Kumar Barik <barikanish0@gmail.com>
2025-06-28 22:01:20 +0530

    diameter of a binary tree - 2 ways

--- dsa/14-Trees/LeetCode/diameter.java
+++ dsa/14-Trees/LeetCode/diameter.java
@@ -37,3 +37,34 @@ class Solution {
         return Math.max(lh, rh) + 1;
     }
 }
+
+// faster approach since it needs only one recursive function and not 2
+class Solution {
+    class Info {
+        int dm;
+        int ht;
+
+        Info(int dm, int ht) {
+            this.dm = dm;
+            this.ht = ht;
+        }
+    }
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        Info res = helper(root);
+        return res.dm;
+    }
+
+    private Info helper(TreeNode root) {
+        if (root == null) {
+            return new Info(0, 0);
+        }
+        Info l_info = helper(root.left);
+        Info r_info = helper(root.right);
+
+        int di = Math.max(Math.max(l_info.dm, r_info.dm), l_info.ht + r_info.ht);
+        int ht = Math.max(l_info.ht, r_info.ht) + 1;
+
+        return new Info(di, ht);
+    }
+}
