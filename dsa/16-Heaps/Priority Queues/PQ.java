dba0047dee228aa2d9576d041fe1bf0a5524edc6
Anish Kumar Barik <barikanish0@gmail.com>
2025-07-06 22:58:17 +0530

    comparable inteface

---
+++ dsa/16-Heaps/Priority Queues/PQ.java
@@ -0,0 +1,34 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+class Student implements Comparable<Student> {
+    String name;
+    int rank;
+
+    Student(String n, int r) {
+        name = n;
+        rank = r;
+    }
+
+    @Override
+    public int compareTo(Student s2) {
+        return this.rank - s2.rank;
+    }
+}
+
+public class PQ {
+    public static void main(String[] args) {
+        // Comparator<Student> comparator = Comparator.comparing(o -> o.rank);
+        PriorityQueue<Student> pq = new PriorityQueue<>();
+
+        pq.add(new Student("A", 3));
+        pq.add(new Student("B", 14));
+        pq.add(new Student("C", 2));
+        pq.add(new Student("D", 55));
+
+        while (!pq.isEmpty()) {
+            System.out.println(pq.peek().name + "->" + pq.peek().rank);
+            pq.remove();
+        }
+    }
+}
