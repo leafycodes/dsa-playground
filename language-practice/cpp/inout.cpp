093653f11b7f032272d46945fce143bd5a684f08
Anish Kumar Barik <barikanish0@gmail.com>
2025-07-07 13:30:21 +0530

    user in/out

---
+++ language-practice/cpp/inout.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>  // this includes all the libraries
+#include <math.h>
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  cout << "hello there" << endl;
+  cout << "hello there";
+
+  int x, y;
+  cin >> x >> y;
+  cout << "value of x: " << x << " and y: " << y;
+
+  return 0;
+}
