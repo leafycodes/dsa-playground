a138ded4023ac28b9747f70049e2213d8e3d962d
Anish Kumar Barik <barikanish0@gmail.com>
2025-07-07 23:16:12 +0530

    leetcode daily

--- language-practice/cp/cpp/type.cpp
+++ language-practice/cp/cpp/type.cpp
@@ -1,17 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void func(int num) { cout << endl << num; }
+class Solution {
+   public:
+    int maxEvents(vector<vector<int>>& events) {
+        if (events.empty()) {
+            return 0;
+        }
 
-int main() {
-    string name;
-    getline(cin, name);
-    cout << "hello, " + name << endl;
-    int x;
-    cin >> x;
-    cout << "the value of x is: " << x;
+        sort(events.begin(), events.end());
 
-    stack<int> s;
-    func(18);
-    return 0;
-}
+        priority_queue<int, vector<int>, greater<int>> min_heap;
+        int max_events = 0;
+        int i = 0;
+        int n = events.size();
+        int current_day = 0;
+
+        while (!min_heap.empty() || i < n) {
+            if (min_heap.empty()) {
+                current_day = events[i][0];
+            }
+
+            while (i < n && events[i][0] <= current_day) {
+                min_heap.push(events[i][1]);
+                i++;
+            }
+
+            min_heap.pop();
+            max_events++;
+            current_day++;
+
+            while (!min_heap.empty() && min_heap.top() < current_day) {
+                min_heap.pop();
+            }
+        }
+
+        return max_events;
+    }
+};
