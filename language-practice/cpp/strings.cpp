98d70ff3ee0295eed86aebe3e74dc28b4b1f9af0
Anish Kumar Barik <barikanish0@gmail.com>
2025-07-07 14:30:59 +0530

    strings, loops

---
+++ language-practice/cpp/strings.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  // string s = "Anish";
+  // cout << s[s.length() - 1];  // .size() also works
+  // s[1] = 'm';
+  // cout << s;
+
+  // for loop
+  // for (int i = 0; i < 10; i++) {
+  //   cout << "Anish" << endl;
+  // }
+
+  // do while loop
+  // int i = 0;
+  // do {
+  //   cout << "Anish" << endl;
+  //   i++;
+  // } while (i < 3);
+
+  // while loop
+  int i = 0;
+  while (i < 3) {
+    cout << "Anish" << endl;
+    i++;
+  }
+
+  return 0;
+}
