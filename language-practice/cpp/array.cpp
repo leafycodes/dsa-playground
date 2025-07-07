38e7db9d5dac6bc6527c9c0e4a693acac468ba41
Anish Kumar Barik <barikanish0@gmail.com>
2025-07-07 14:17:25 +0530

    arrays and 2d arrats

---
+++ language-practice/cpp/array.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int arr[5];
+  cin >> arr[0] >> arr[1] >> arr[2] >> arr[3] >> arr[4];
+
+  arr[3] += 6;
+  cout << arr[3];
+
+  // 2D arrays
+  int matrix[3][5];
+  matrix[3][1] = 23;
+  cout << endl << matrix[3][1];
+  cout << matrix[0][0];
+
+  return 0;
+}
