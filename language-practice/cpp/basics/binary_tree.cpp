2ba4eaf0c88620f346a38e6e7649ebc3d05ce3e0
Anish Kumar Barik <barikanish0@gmail.com>
2025-07-08 12:12:10 +0530

    BT

--- language-practice/cpp/basics/binary_tree.cpp
+++ language-practice/cpp/basics/binary_tree.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 struct Node {
     int data;
-    struct Node* left;
-    struct Node* right;
+    Node* left;
+    Node* right;
 
     Node(int data) : left(nullptr), right(nullptr) { this->data = data; }
 };
