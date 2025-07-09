3b8602d43729483cca653178aab0481f34f4b9ef
Anish Kumar Barik <barikanish0@gmail.com>
2025-07-10 00:31:01 +0530

    kth smallest in array

--- dsa/16-Heaps/GeeksForGeeks/kthSmallest.cpp
+++ dsa/16-Heaps/GeeksForGeeks/kthSmallest.cpp
@@ -1,13 +1,24 @@
-// User function template for C++
 #include <bits/stdc++.h>
 using namespace std;
 
-// kth smallest or largest - logic remains ame with minor changes
-class Solution {
-   public:
-    // arr : given array
-    // k : find kth smallest element and return using this function
-    int kthSmallest(vector<int> &arr, int k) {
-        // code here
+// Function to find kth smallest array element
+void kthSmallest(vector<int>& v, int N, int K) {
+    // Implement Max Heap using
+    // a Priority Queue
+    priority_queue<int> heap1;
+
+    for (int i = 0; i < N; ++i) {
+        // Insert elements into
+        // the priority queue
+        heap1.push(v[i]);
+
+        // If size of the priority
+        // queue exceeds k
+        if (heap1.size() > K) {
+            heap1.pop();
+        }
     }
-};
+
+    // Print the k-th smallest element
+    cout << heap1.top() << endl;
+}
