9fabaa7b36db0f1f7e7a91b077d6251ad257e950
Anish Kumar Barik <barikanish0@gmail.com>
2025-07-11 15:41:00 +0530

    hashmap impl - array of linked list

---
+++ dsa/17-Hashing/Maps/Implementation/GeeksForGeeks (int, int) - C++/myHashMap.cpp
@@ -0,0 +1,18 @@
+// User function Template for C++
+
+class MyHashMap {
+   public:
+    /** Initialize your data structure here. */
+    MyHashMap() {}
+
+    /** value will always be non-negative. */
+    void put(int key, int value) {}
+
+    /** Returns the value to which the specified key is mapped, or -1 if this
+     * map contains no mapping for the key */
+    int get(int key) {}
+
+    /** Removes the mapping of the specified value key if this map contains a
+     * mapping for the key */
+    void remove(int key) {}
+};
--- language-practice/cp/cpp/type.cpp
+++ language-practice/cp/cpp/type.cpp
@@ -2,16 +2,31 @@
 using namespace std;
 
 int main() {
-    int n;
-    cin >> n;
-    long long total = 1LL * n * (n + 1) / 2;
-    long long sum = 0;
-    for (int i = 0; i < n - 1; i++) {
-        int temp;
-        cin >> temp;
-        sum += temp;
+    string str;
+    getline(cin, str);
+    int n = str.length();
+
+    if (n == 1) {
+        cout << 1;
+        return 0;
     }
-    cout << total - sum << endl;
+
+    int max_count = 1;
+    int curr_count = 1;
+    char prev = str[0];
+    for (int i = 1; i < n; i++) {
+        if (str[i] == prev) {
+            curr_count += 1;
+        } else {
+            curr_count = 1;
+        }
+
+        max_count = max(max_count, curr_count);
+        prev = str[i];
+    }
+
+    cout << max_count;
+
     return 0;
 }
 
