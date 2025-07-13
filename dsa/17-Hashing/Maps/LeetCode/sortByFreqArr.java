03dd5cef4df59e204291a8f1d495b0f7aae61edf
Anish Kumar Barik <barikanish0@gmail.com>
2025-07-13 16:59:36 +0530

    sort array by frequency of elements

---
+++ dsa/17-Hashing/Maps/LeetCode/sortByFreqArr.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+class Solution {
+    public int[] frequencySort(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        List<Integer> list = new ArrayList<>();
+        for (int num : nums) {
+            list.add(num);
+        }
+
+        Collections.sort(list, (a, b) -> {
+            int freqA = map.get(a);
+            int freqB = map.get(b);
+            if (freqA != freqB) {
+                return freqA - freqB;
+            } else {
+                return b - a;
+            }
+        });
+
+        int[] result = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
+
+        return result;
+    }
+}
