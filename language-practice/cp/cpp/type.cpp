1b67364886b6c6b38b9a400dec855b1df4c74eb5
Anish Kumar Barik <barikanish0@gmail.com>
2025-07-22 02:25:54 +0530

    leetcode

--- language-practice/cp/cpp/type.cpp
+++ language-practice/cp/cpp/type.cpp
@@ -1,21 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution {
+   public:
+    string removeDuplicates(string str, int k) {
+        stack<pair<char, int>> s;
+
+        for (int i = 0; i < str.length(); ++i) {
+            if (s.empty() || s.top().first != str[i]) {
+                s.push({str[i], 1});
+            } else {
+                ++s.top().second;
+
+                if (s.top().second >= k) {
+                    s.pop();
+                }
+            }
+        }
+
+        string res;
+        while (!s.empty()) {
+            pair<char, int> curr = s.top();
+            s.pop();
+
+            for (int i = 0; i < curr.second; i++) {
+                res += curr.first;
+            }
+        }
+
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
+
 int main() {
     string str;
-    getline(cin, str);
-
-    int count = 0;
-    queue<char> q;
-    for (int i = 0; i < str.length(); i++) {
-        if (str[i] == '`' && q.empty()) {
-            q.push(str[i]);
-        } else if (str[i] == '`') {
-            q.pop();
-            count++;
-        }
-    }
-
-    cout << count;
+    cin >> str;
+    Solution s;
+    cout << s.removeDuplicates("deeedbbcccbdaa", 3);
     return 0;
 }
--- language-practice/cp/java/Type.java
+++ language-practice/cp/java/Type.java
@@ -1,21 +1,47 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+import java.util.Scanner;
+import java.util.Stack;
+
+class Pair {
+    char ch;
+    int fq;
+
+    Pair(char ch, int fq) {
+        this.ch = ch;
+        this.fq = fq;
+    }
+}
+
 class Solution {
-    public int getDecimalValue(ListNode head) {
-        ListNode curr = head;
-        int num = 0;
-        while (curr != null) {
-            num = ((num << 1) | curr.val);
-            curr = curr.next;
+    public static String makeFancyString(String s) {
+        Stack<Pair> st = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (st.isEmpty() || st.peek().ch != s.charAt(i)) {
+                st.push(new Pair(s.charAt(i), 1));
+            } else if (st.peek().ch == s.charAt(i)) {
+                st.peek().fq++;
+                if (st.peek().fq >= 3) {
+                    st.peek().fq = 2;
+                }
+            }
         }
-        return num;
+
+        StringBuilder res = new StringBuilder();
+        while (!st.isEmpty()) {
+            Pair curr = st.pop();
+            for (int i = 0; i < curr.fq; i++) {
+                res.append(curr.ch);
+            }
+        }
+
+        return res.reverse().toString();
+    }
+}
+
+public class Type {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(Solution.makeFancyString(str));
+        sc.close();
     }
 }
