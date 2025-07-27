c1570e7e648022be4d0a979a14a82e6c09aee40f
Anish Kumar Barik <barikanish0@gmail.com>
2025-07-27 23:35:46 +0530

    dijkstra's algorithm: shortest path from src to all vertices

--- dsa/19-Graphs/GeeksForGeeks/dijkstra.cpp
+++ dsa/19-Graphs/GeeksForGeeks/dijkstra.cpp
@@ -1,10 +1,48 @@
 #include <bits/stdc++.h>
+#define pii pair<int, int>
 using namespace std;
 
-// User Function Template
+class compare {
+   public:
+    bool operator()(const pii& a, const pii& b) { return a.second > b.second; }
+};
+
 class Solution {
    public:
-    vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
-        // Code here
+    vector<int> dijkstra(int V, vector<vector<int>>& edges, int src) {
+        vector<vector<pii>> graph(V);
+        for (const auto& edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            int wt = edge[2];
+            graph[u].push_back({v, wt});
+        }
+
+        vector<int> dist(V, INT_MAX);
+        dist[src] = 0;
+        vector<bool> vis(V, false);
+        priority_queue<pii, vector<pii>, compare> pq;
+        pq.push({src, 0});
+
+        // bfs loop
+        while (!pq.empty()) {
+            pii curr = pq.top();
+            pq.pop();
+
+            if (!vis[curr.first]) {
+                vis[curr.first] = true;
+                for (int i = 0; i < graph[curr.first].size(); i++) {
+                    int u = curr.first;
+                    int v = graph[curr.first][i].first;
+                    int wt = graph[curr.first][i].second;
+                    if (dist[v] > dist[u] + wt) {
+                        dist[v] = dist[u] + wt;
+                        pq.push({v, dist[v]});
+                    }
+                }
+            }
+        }
+
+        return dist;
     }
 };
