16579a211796b4c8d8b01ae4646e832b0ef390a0
Anish Kumar Barik <barikanish0@gmail.com>
2025-07-29 02:11:36 +0530

    course schedule: topological sort

---
+++ dsa/19-Graphs/LeetCode/schedule.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        int in[] = new int[numCourses];
+        for (int i = 0; i < prerequisites.length; i++) {
+            int u = prerequisites[i][0];
+            int v = prerequisites[i][1];
+
+            // u,v -> take v before , i.e. v -> u
+            graph.get(v).add(u);
+            in[u]++;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (in[i] == 0) {
+                q.add(i);
+            }
+        }
+
+        ArrayList<Integer> res = new ArrayList<>();
+        while (!q.isEmpty()) {
+            int curr = q.remove();
+            res.add(curr);
+
+            for (int ngbr : graph.get(curr)) {
+                in[ngbr]--;
+                if (in[ngbr] == 0) {
+                    q.add(ngbr);
+                }
+            }
+        }
+
+        return res.size() == numCourses ? true : false;
+    }
+}
