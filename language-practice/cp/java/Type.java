39b73ad5b811ef88aa2db1bf140f512ec91255fe
Anish Kumar Barik <barikanish0@gmail.com>
2025-08-01 14:59:11 +0530

    cses

--- language-practice/cp/java/Type.java
+++ language-practice/cp/java/Type.java
@@ -1,46 +1,30 @@
-import java.util.HashSet;
-import java.util.Set;
-
-class Solution {
-    public int maxSum(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-
-        boolean allNeg = true;
-        int maxNeg = Integer.MIN_VALUE;
-        for (int num : nums) {
-            if (num >= 0) {
-                allNeg = false;
-            }
-            maxNeg = Math.max(maxNeg, num);
-        }
-
-        if (allNeg) {
-            return maxNeg;
-        }
-
-        Set<Integer> set = new HashSet<>();
-        int max = 0;
-
-        for (int j = 0; j < nums.length; j++) {
-            if (nums[j] > 0) {
-                if (!set.contains(nums[j])) {
-                    set.add(nums[j]);
-                    max += nums[j];
-                }
-            }
-        }
-
-        return max;
-    }
-}
+import java.util.Scanner;
 
 public class Type {
     public static void main(String[] args) {
-        int nums[] = { -95, 23, 23, -94 };
-        Solution s = new Solution();
-        int res = s.maxSum(nums);
-        System.out.println(res);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        sc.close();
+
+        System.out.println(moves(nums));
+    }
+
+    public static long moves(int[] nums) {
+        long move = 0;
+        int prev = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < prev) {
+                move += prev - nums[i];
+            } else {
+                prev = nums[i];
+            }
+        }
+
+        return move;
     }
 }
