5eb58104456918bf30645d1ef8d4ebb4d66b22ff
Anish Kumar Barik <barikanish0@gmail.com>
2025-08-20 01:41:39 +0530

    house robber dp

--- dsa/20-Dynamic Programming/LeetCode/houseRobber.java
+++ dsa/20-Dynamic Programming/LeetCode/houseRobber.java
@@ -4,6 +4,63 @@ class Solution {
             return 0;
         }
 
+        return helper(nums.length - 1, nums);
+    }
+
+    private int helper(int n, int nums[]) {
+        if (n < 0) {
+            return 0;
+        }
+
+        if (n == 0) {
+            return nums[0];
+        }
+
+        int val1 = helper(n - 1, nums);
+        int val2 = nums[n] + helper(n - 2, nums);
+
+        return Math.max(val1, val2);
+    }
+}
+
+// memoization dp -> still TLE for 3 cases
+class Solution2a {
+    public int rob(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int dp[] = new int[101];
+        return helper(nums.length - 1, nums, dp);
+    }
+
+    private int helper(int n, int nums[], int[] dp) {
+        if (n < 0) {
+            return 0;
+        }
+
+        if (n == 0) {
+            return nums[0];
+        }
+
+        if (dp[n] != 0) {
+            return dp[n];
+        }
+
+        int val1 = helper(n - 1, nums, dp);
+        int val2 = nums[n] + helper(n - 2, nums, dp);
+
+        return dp[n] = Math.max(val1, val2);
+    }
+}
+
+// tabulation dp -> AC
+class Solution2b {
+    public int rob(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
         if (nums.length == 1) {
             return nums[0];
         }
