9cb0ea7f87303082e43c2d48f31ce4745740313f
Anish Kumar Barik <barikanish0@gmail.com>
2025-09-06 20:04:39 +0530

    house robber

--- dsa/20-Dynamic Programming/LeetCode/houseRobber.java
+++ dsa/20-Dynamic Programming/LeetCode/houseRobber.java
@@ -79,3 +79,29 @@ class Solution2b {
         return dp[nums.length - 1];
     }
 }
+
+// tabualtion reverse
+class Solution4 {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int dp[] = new int[n];
+
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return nums[0];
+        }
+
+        dp[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            if (i == n - 2) {
+                dp[i] = Math.max(dp[i + 1], nums[i]);
+                continue;
+            }
+
+            dp[i] = Math.max(dp[i + 1], nums[i] + dp[i + 2]);
+        }
+
+        return dp[0];
+    }
+}
