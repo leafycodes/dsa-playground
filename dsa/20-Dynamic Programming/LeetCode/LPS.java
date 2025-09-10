47ff5237463a2bd2a31a7f446f993c5bec2584ca
Anish Kumar Barik <barikanish0@gmail.com>
2025-09-11 02:03:30 +0530

    longest palindromic subsequence

--- dsa/20-Dynamic Programming/LeetCode/LPS.java
+++ dsa/20-Dynamic Programming/LeetCode/LPS.java
@@ -26,3 +26,30 @@ class Solution {
         return dp[0][n - 1];
     }
 }
+
+// way 2 -> simply lcs of str and reverse str
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        StringBuilder t = new StringBuilder(s);
+        String s2 = new String(t.reverse());
+        return lcs(s, s2);
+    }
+
+    private int lcs(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+        int dp[][] = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+}
