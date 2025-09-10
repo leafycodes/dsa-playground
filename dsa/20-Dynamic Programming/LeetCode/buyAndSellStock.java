ae00cb6d5d4584a6d7856a551bf00439bf7d0fa4
Anish Kumar Barik <barikanish0@gmail.com>
2025-09-11 01:59:05 +0530

    buy and sell stock with transaction fee

--- dsa/20-Dynamic Programming/LeetCode/buyAndSellStock.java
+++ dsa/20-Dynamic Programming/LeetCode/buyAndSellStock.java
@@ -11,3 +11,38 @@ class Solution {
         return sell;
     }
 }
+
+// way 2 -> more intuitive
+class Solution {
+    public int maxProfit(int[] prices, int fee) {
+        int n = prices.length;
+        int[][] memo = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(memo[i], -1);
+        }
+        return helper(prices, 0, 1, fee, memo);
+    }
+
+    private int helper(int[] prices, int i, int buy, int fee, int[][] memo) {
+        if (i == prices.length) {
+            return 0;
+        }
+
+        if (memo[i][buy] != -1) {
+            return memo[i][buy];
+        }
+
+        int profit;
+        if (buy == 1) {
+            int take = -prices[i] + helper(prices, i + 1, 0, fee, memo);
+            int leave = helper(prices, i + 1, 1, fee, memo);
+            profit = Math.max(take, leave);
+        } else {
+            int sell = prices[i] - fee + helper(prices, i + 1, 1, fee, memo);
+            int hold = helper(prices, i + 1, 0, fee, memo);
+            profit = Math.max(sell, hold);
+        }
+
+        return memo[i][buy] = profit;
+    }
+}
