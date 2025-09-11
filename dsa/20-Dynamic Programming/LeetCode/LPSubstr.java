ebb16ed88fb1c430591d98a5c1e9f04108bb605b
Anish Kumar Barik <barikanish0@gmail.com>
2025-09-11 13:43:31 +0530

    longest palindromic substring

---
+++ dsa/20-Dynamic Programming/LeetCode/LPSubstr.java
@@ -0,0 +1,40 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        if (n <= 1) {
+            return s;
+        }
+
+        boolean[][] dp = new boolean[n][n];
+        int left = 0;
+        int maxLen = 1;
+
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                dp[i][i + 1] = true;
+                left = i;
+                maxLen = 2;
+            }
+        }
+
+        for (int len = 3; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+
+                if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]) {
+                    dp[i][j] = true;
+                    if (len > maxLen) {
+                        left = i;
+                        maxLen = len;
+                    }
+                }
+            }
+        }
+
+        return s.substring(left, left + maxLen);
+    }
+}
